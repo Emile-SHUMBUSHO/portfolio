78347c594b0040c04d7fa224b450cfb6810254a7
Emile-ALX <shumbushoemilef1@gmail.com>
2022-05-23 10:50:38 +0200

    update

---
+++ scripts.js
@@ -0,0 +1,23 @@
+let themeDotes = document.getElementsByClassName('theme-dot')
+for(let i=0; i<themeDotes.length; i++){
+    themeDotes[i].addEventListener('click', function(){
+        let mode = this.dataset.mode
+        console.log('button clicked:', mode)
+        setTheme(mode)
+    })
+}
+
+function setTheme(mode){
+    if(mode == 'light'){
+        document.getElementById('theme-style').href = 'default-css.css'
+    }
+    if(mode == 'blue'){
+        document.getElementById('theme-style').href = 'blue.css'
+    }
+    if(mode == 'green'){
+        document.getElementById('theme-style').href = 'green.css'
+    }
+    if(mode == 'purple'){
+        document.getElementById('theme-style').href = 'purple.css'
+    }
+}
